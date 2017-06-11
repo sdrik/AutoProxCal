039b4600633ef9dd515c8a823227e592e577c10a
Cédric Schieli <cschieli@gmail.com>
2017-06-11 19:26:41 +0200

    Initial commit

---
+++ app/src/main/java/fr/gabriello/autoproxcal/MainActivity.java
@@ -0,0 +1,109 @@
+package fr.gabriello.autoproxcal;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Vibrator;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    private ToggleButton mTestButton;
+
+    public static class PrefsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+        TextView value = (TextView)findViewById(R.id.sensorNameValue);
+        value.setText(mSensor.getName());
+        value = (TextView)findViewById(R.id.sensorVendorValue);
+        value.setText(mSensor.getVendor());
+
+        mTestButton = (ToggleButton)findViewById(R.id.testButton);
+        mTestButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    enableProximitySensor();
+                } else {
+                    disableProximitySensor();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mTestButton.isChecked()) {
+            enableProximitySensor();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        disableProximitySensor();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor.getType() == Sensor.TYPE_PROXIMITY) {
+            View layout = findViewById(R.id.mainLayout);
+            Vibrator vibrator = (Vibrator)getSystemService(VIBRATOR_SERVICE);
+            if (event.values[0] == 0) {
+                layout.setBackgroundColor(getResources().getColor(R.color.colorNear));
+                vibrator.vibrate(999999);
+            } else {
+                layout.setBackgroundColor(Color.WHITE);
+                vibrator.cancel();
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    public void onClickCalibrateButton(View view) {
+        startService(new Intent(this, ProximityCalibrateService.class));
+    }
+
+    private void enableProximitySensor() {
+        mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    private void disableProximitySensor() {
+        mSensorManager.unregisterListener(this);
+        Vibrator vibrator = (Vibrator)getSystemService(VIBRATOR_SERVICE);
+        vibrator.cancel();
+        View layout = findViewById(R.id.mainLayout);
+        layout.setBackgroundColor(Color.WHITE);
+    }
+
+}
---
+++ app/src/main/java/fr/gabriello/autoproxcal/ProximityCalibrateService.java
@@ -0,0 +1,70 @@
+package fr.gabriello.autoproxcal;
+
+import android.app.IntentService;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.preference.PreferenceManager;
+import android.widget.Toast;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class ProximityCalibrateService extends IntentService {
+
+    public ProximityCalibrateService() {
+        super("ProximityCalibrateService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent == null) return;
+
+        SensorManager sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        SensorEventListener listener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {}
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+        };
+
+        boolean success = false;
+        sensorManager.registerListener(listener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+        try {
+            Process process = Runtime.getRuntime().exec("su");
+            DataOutputStream STDIN = new DataOutputStream(process.getOutputStream());
+            STDIN.write("echo 0 > /sys/devices/virtual/sensors/proximity_sensor/prox_cal && echo 1 > /sys/devices/virtual/sensors/proximity_sensor/prox_cal; exit $?\n".getBytes());
+            STDIN.flush();
+            success = (process.waitFor() == 0);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        sensorManager.unregisterListener(listener);
+
+        if (success) {
+            Application.toast(this, R.string.calibration_success, Toast.LENGTH_SHORT);
+        } else {
+            Application.toast(this, R.string.calibration_failure, Toast.LENGTH_LONG);
+        }
+    }
+
+    public static class BootCompletedReceiver extends BroadcastReceiver {
+        private static final String PREFS_CALIBRATE_ON_BOOT = "calibrate_on_boot";
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (context == null) return;
+            SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
+            if ((settings != null) && settings.getBoolean(PREFS_CALIBRATE_ON_BOOT, false)) {
+                context.startService(new Intent(context, ProximityCalibrateService.class));
+            }
+        }
+    }
+
+}
